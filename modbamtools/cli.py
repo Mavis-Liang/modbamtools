b0ea9b23190671f61e13bab308421cd2c29a3d4b
Mavis Liang <liangxw27@gmail.com>
2023-01-18 15:57:48 -0500

    fix bug

--- modbamtools/cli.py
+++ modbamtools/cli.py
@@ -795,7 +795,7 @@ def print_reads(
             with ModBam(bam) as b:
                 for read in b.reads(chrom, start, end):
                     for pos_mod in read.mod_sites:
-                        r.write(str(*pos_mod[1:]))
+                        r.write(str(pos_mod[1:])+"\n")
                         #pos_mod.to_csv(r, sep='\t', index=False)
                         #r.write("\n")
         
